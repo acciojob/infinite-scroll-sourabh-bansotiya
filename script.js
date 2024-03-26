9b448f5bb9836f80fd4edb31ba581c7fa229e6e1
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-03-27 00:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 //your code here!
 let list = document.querySelector("#infiList");
 
-list.innerText = "
-	      <li>Item 1</li>
-		  <li>Item 2</li>
-		  <li>Item 3</li>
-		  <li>Item 4</li>
-		  <li>Item 5</li>
-		  <li>Item 6</li>
-		  <li>Item 7</li>
-		  <li>Item 8</li>
-		  <li>Item 9</li>
-		  <li>Item 10</li>
-		  <li>Item 11</li>
-		  <li>Item 12</li>
-		
-	";
+let addItems = (start, end) => {
+	for (let i = start; i < end; i++) {
+		let listItems = document.createElement('li');
+		listItems.textContent = `Items ${i}`;
+		list.appendChild(listItems);
+	}
+};
 
-function infiList() {
-             const para = document.createElement("li");
-	         const paraText = document.createTextNode("Sourabh")
-             para.appendChild (para);
-             document.body.appendChild(para);
-            }
+addItems(1,13);
+
+list.addEventListener('scroll',() => {
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+
+		const currItems = list.childElementCount;
+		addItems(currItems + 1, currItems + 2);
+
+	}
+})
 
 
