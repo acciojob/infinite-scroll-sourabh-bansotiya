14acbcc14a196d83ca2879b120ffc926888c183c
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-03-26 23:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
+let list = document.querySelector("#infiList");
+
+list.innerText = "
+	      <li>Item 1</li>
+		  <li>Item 2</li>
+		  <li>Item 3</li>
+		  <li>Item 4</li>
+		  <li>Item 5</li>
+		  <li>Item 6</li>
+		  <li>Item 7</li>
+		  <li>Item 8</li>
+		  <li>Item 9</li>
+		  <li>Item 10</li>
+		  <li>Item 11</li>
+		  <li>Item 12</li>
+		
+	";
+
+function infiList() {
+             const para = document.createElement("li");
+	         const paraText = document.createTextNode("Sourabh")
+             para.appendChild (para);
+             document.body.appendChild(para);
+            }
+
 
